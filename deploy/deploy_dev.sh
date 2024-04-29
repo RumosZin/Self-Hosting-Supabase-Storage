dc3093dfae0eee87eeccb7065d42ebb5594eb578
rumos <gyunjin3@gmail.com>
2024-04-29 22:57:37 +0900

    배포 서버 docker compose up 스크립트 작성

---
+++ deploy/deploy_dev.sh
@@ -0,0 +1,3 @@
+sh docker_image_load.sh
+
+docker compose -f ../docker-compose.yml up -d --build
---
+++ deploy/docker_image_load.sh
@@ -0,0 +1,6 @@
+docker load -i images/supabase-studio:20240415-304bec8.tar.gz
+docker load -i images/kong:2.8.1.tar.gz
+docker load -i images/postgrest-postgrest:v12.0.1.tar.gz
+docker load -i images/supabase-storage-api:v1.0.10.tar.gz
+docker load -i images/supabase-postgres-meta.tar.gz
+docker load -i images/supabase-postgres:15.1.1.42.tar.gz
